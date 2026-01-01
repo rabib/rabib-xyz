fc3f0a72095e878f14aa211de7f63b2430f721fb
rabib <53358432+rabib@users.noreply.github.com>
2025-12-31 22:20:34 -0500

    deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,8 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-    site: 'https://rabib.xyz',
+    site: 'https://rabib.github.io',
+    base: '/rabib-xyz'
     markdown: {
         syntaxHighlight: false
     }
